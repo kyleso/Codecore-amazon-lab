840beaaa9a4a555203f4faad6f5de6122a96a9b3
Kyle So <kyle.p.so@gmail.com>
2019-02-14 13:50:58 -0800

    add contact_us page and added link in nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get('home', to: "welcome#home")
   get('about', to: "welcome#about")
+  get('contact_us', to: "welcome#contact_us")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
