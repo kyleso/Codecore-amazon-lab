04da9b087c80cf661cbb72eb4ff29438faacb34d
Kyle So <kyle.p.so@gmail.com>
2019-02-14 14:14:08 -0800

    create contact_us view, route, with thank_you view after submission

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,16 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def contact_us
+    @full_name = params[:full_name]
+    @email = params[:email]
+    @message = params[:message]
+
+    render :thank_you
+  end
+
+  def bill_splitter
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get('home', to: "welcome#home")
-  get('about', to: "welcome#about")
-  get('contact_us', to: "welcome#contact_us")
+  get('/home', to: "welcome#home")
+  get('/about', to: "welcome#about")
+  get('/contact_us', to: "welcome#contact_us")
+  post("/contact_us", to: "welcome#thank_you")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
