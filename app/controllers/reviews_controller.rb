6de2af4ac508bae1a593fb25249933db45d252a7
Kyle So <kyle.p.so@gmail.com>
2019-02-22 19:33:45 -0800

    fix review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,7 +17,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find params[:id]
     @review.destroy
-    redirect_to product_url(@review.question)
+    redirect_to product_url(@review.product)
   end
 
   private
